564200392cb6d75669c6a1a5f2b1ee81f1bec3cc
Mahendra <manishtoons@users.noreply.github.com>
2021-04-21 23:03:51 +0530

    ui tweaks

--- python/playblast/dialog.py
+++ python/playblast/dialog.py
@@ -8,17 +8,16 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
-
-import pprint
-
 # by importing QT from sgtk rather than directly, we ensure that
 # the code will be compatible with both PySide and PyQt.
 import sgtk
+import traceback
 from sgtk.platform.qt import QtCore, QtGui
 from .ui.dialog import Ui_Dialog
 from .playblast import PlayblastManager
 
 logger = sgtk.platform.get_logger(__name__)
+camera_utils = sgtk.platform.import_framework("tvfx-maya-utils", "camera_utils")
 
 
 def show_dialog(app_instance, entity_type, entity_ids, version_str='0.0.1'):
@@ -55,6 +54,15 @@ class AppDialog(QtGui.QWidget):
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
 
+        self.ui.cb_pass_type.currentTextChanged.connect(self._toggle_custom_pass_type)
+        self.ui.cb_camera_type.currentTextChanged.connect(self._toggle_custom_camera_type)
+        self.ui.pb_cancel.released.connect(self.deleteLater)
+        self.ui.le_pass_type_custom.setVisible(False)
+        self.ui.le_camera_custom.setVisible(False)
+
+        self.ui.le_frame_start.setValidator(QtGui.QIntValidator(self))
+        self.ui.le_frame_end.setValidator(QtGui.QIntValidator(self))
+
         # most of the useful accessors are available through the Application class instance
         # it is often handy to keep a reference to this. You can get it via the following method:
         self._app = sgtk.platform.current_bundle()
@@ -77,6 +85,18 @@ class AppDialog(QtGui.QWidget):
         # self.ui.context.setText("Current selection type: %s, <br>Currently selected ids: %s" % (entity_type, entity_ids))
         self.ui.createPlayblast.clicked.connect(self.do_playblast)
 
+    def _toggle_custom_pass_type(self, val):
+        if val == 'Custom':
+            self.ui.le_pass_type_custom.setVisible(True)
+        else:
+            self.ui.le_pass_type_custom.setVisible(False)
+
+    def _toggle_custom_camera_type(self, val):
+        if val == 'Custom':
+            self.ui.le_camera_custom.setVisible(True)
+        else:
+            self.ui.le_camera_custom.setVisible(False)
+
     def set_default_ui_data(self):
         """
         method to set defaults in ui to minimize user input.
@@ -87,40 +107,57 @@ class AppDialog(QtGui.QWidget):
             start_frame, end_frame = self.pbMngr.get_frame_range()
             self._app.logger.info("set_default_ui_data: start_frame, end_frame = {0}, {1}".format(start_frame,
                                                                                                   end_frame))
-            self.ui.frameRange_start.setText(str(start_frame))
-            self.ui.frameRange_end.setText(str(end_frame))
+            self.ui.le_frame_start.setText(str(start_frame))
+            self.ui.le_frame_end.setText(str(end_frame))
 
             # SCALE:
-            self.ui.lineEdit_scale.setText(str('0.50'))
+            self.ui.sb_scale.setValue(0.5)
 
             # FRAME PADDING: how many frames before the start frame
-            self.ui.lineEdit_framePadding.setText(str('4'))
+            self.ui.sb_padding.setValue(4)
+            self.ui.sb_focal.setValue(35)
 
             # RESOLUTION : (hard coding for now)
-            self.ui.lineEdit_finalRes_w.setText(str('960'))
-            self.ui.lineEdit_finalRes_h.setText(str('540'))
+            self.ui.sb_res_w.setValue(960)
+            self.ui.sb_res_h.setValue(540)
 
             # FORMAT: default playblast format is set to movie as of now
-            self.ui.cb_format.setCurrentText(self.ui.cb_format.keys().index('avi'))
-            # self.ui.cb_encoding.setCurrentText(self.ui.cb_encoding.keys().index('None'))
+            self.ui.cb_format.setCurrentIndex(0)
 
             # pass type
-            self.ui.cb_passType.setCurrentText(self.ui.cb_passType.keys().index('smoothShaded'))
-            self.pbMngr.set_pass_type(str(self.ui.cb_passType.currentText()))
+            self.ui.cb_pass_type.setCurrentIndex(0)
+            self.pbMngr.set_pass_type(str(self.ui.cb_pass_type.currentText()))
 
             # camera type
-            self.ui.cb_cameraType.setCurrentText(self.ui.cb_cameraType.keys().index('perspective'))
-            self.pbMngr.set_camera_type(str(self.ui.cb_cameraType.currentText()))
+            self.ui.cb_camera_type.setCurrentIndex(0)
+            self.pbMngr.set_camera_type(str(self.ui.cb_camera_type.currentText()))
 
             # description
             context = self.pbMngr.get_context()
-            # self.ui.textEdit_comment.setText("playblast for {0}, {1}".format(comment_entity, comment_project))
-            self.ui.plainTextEdit_comment.setPlainText("playblast for {0}, {1}".format(context.entity, context.project))
             self._app.logger.debug(
                 "set_default_ui_data: playblast for {0}, {1}".format(context.entity, context.project))
 
-        except:
-            self._app.logger.debug("Could not set the ui defaults -_-")
+            self.ui.le_comments.setText(','.join(camera_utils.get_camera_pattern()))
+
+        except Exception as err:
+            print(traceback.format_exc())
+            self._app.logger.debug("Could not set the ui defaults: {}".format(err))
+
+    @property
+    def camera_type(self):
+        camera_type = self.cb_camera_type.currentText()
+        if camera_type == "Custom":
+            camera_type = self.le_camera_custom.text()
+
+        return camera_type
+
+    @property
+    def pass_type(self):
+        pass_type = self.cb_pass_type.currentText()
+        if pass_type == 'Custom':
+            pass_type = self.le_pass_type_custom.text()
+
+        return pass_type
 
     def gatherUiData(self):
         """
@@ -128,35 +165,35 @@ class AppDialog(QtGui.QWidget):
         :return:
         """
         playblastParams = {
-            'startTime': int(self.ui.frameRange_start.text()),
-            'endTime': int(self.ui.frameRange_end.text()),
+            'startTime': int(self.ui.le_frame_start.text()),
+            'endTime': int(self.ui.le_frame_end.text()),
             'forceOverwrite': True,
             'format': str(self.ui.cb_format.currentText()),
-            'percent': float(self.ui.lineEdit_scale.text()) * 100,
+            'percent': float(self.ui.sb_scale.value()) * 100,
             # 'compression': 'H.264',
-            'width': int(float(self.ui.lineEdit_finalRes_w.text())),
-            'height': int(float(self.ui.lineEdit_finalRes_h.text())),
+            'width': int(float(self.ui.sb_res_w.value())),
+            'height': int(float(self.ui.sb_res_h.value())),
             'offScreen': True,
             # 'quality': 70,
             # 'viewer': True,
-            'framePadding': int(self.ui.lineEdit_framePadding.text()),
+            'framePadding': int(self.ui.sb_padding.value()),
             # 'filename': maya_output,
             'filename': self.pbMngr.get_temp_output('.avi')  # self.pbMngr.format_output_path(),
             # 'compression': encoding
         }
-        self.pbMngr.set_pass_type(str(self.ui.cb_passType.currentText()))
-        self.pbMngr.set_description(str(self.ui.plainTextEdit_comment.toPlainText()))
-        self.pbMngr.set_camera_type(str(self.ui.cb_cameraType.currentText()))
+        self.pbMngr.set_pass_type(self.pass_type)
+        self.pbMngr.set_camera_type(self.camera_type)
+        self.pbMngr.set_description(str(self.ui.le_comments.text()))
 
         self._app.logger.debug("playblastParams gathered: ")
         self._app.logger.debug(playblastParams)
 
-        self._app.logger.debug("cb_passType.currentText= {}".format(str(self.ui.cb_passType.currentText())))
-        self._app.logger.debug("plainTextEdit_comment= {}".format(str(self.ui.plainTextEdit_comment.toPlainText())))
-        self._app.logger.debug("cb_cameraType.currentText= {}".format(str(self.ui.cb_cameraType.currentText())))
+        self._app.logger.debug("cb_passType.currentText= {}".format(str(self.ui.cb_pass_type.currentText())))
+        self._app.logger.debug("plainTextEdit_comment= {}".format(str(self.ui.le_comments.text())))
+        self._app.logger.debug("cb_cameraType.currentText= {}".format(str(self.ui.cb_camera_type.currentText())))
 
         # self._app.logger.debug("playblastParams['filename'] = {} ".format(playblastParams['filename']))
-        pprint.pprint(playblastParams)
+        logger.info(playblastParams)
         return playblastParams
 
     def do_playblast(self):
